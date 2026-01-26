6bc1f9f0c97b21e1c68126477eefccfa428f16fd
webmancer1 <mwendwajames2004@gmail.com>
2026-01-26 17:43:45 +0300

    profile page updates

---
+++ app/src/main/java/com/example/lifeping/data/model/UserProfile.kt
@@ -0,0 +1,9 @@
+package com.example.lifeping.data.model
+
+data class UserProfile(
+    val uid: String = "",
+    val fullName: String = "",
+    val email: String = "",
+    val bio: String = "",
+    val profilePictureUrl: String = ""
+)
