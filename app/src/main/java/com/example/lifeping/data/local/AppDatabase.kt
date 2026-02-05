90551726d225d44e904fd7c2fc1654744b05cfe4
webmancer1 <mwendwajames2004@gmail.com>
2026-02-05 12:40:56 +0300

    database logic

--- app/src/main/java/com/example/lifeping/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/lifeping/data/local/AppDatabase.kt
@@ -3,8 +3,10 @@ package com.example.lifeping.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.lifeping.data.model.Contact
+import com.example.lifeping.data.model.CheckIn
 
-@Database(entities = [Contact::class], version = 2, exportSchema = false)
+@Database(entities = [Contact::class, CheckIn::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun contactDao(): ContactDao
+    abstract fun checkInDao(): CheckInDao
 }
