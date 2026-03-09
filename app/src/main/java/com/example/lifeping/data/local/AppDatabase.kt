ae9463c03f982c7433f88ab1d1e94aac29e3b65e
webmancer1 <mwendwajames2004@gmail.com>
2026-03-09 18:10:00 +0300

    overdue notifications

--- app/src/main/java/com/example/lifeping/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/lifeping/data/local/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.lifeping.data.model.Contact
 import com.example.lifeping.data.model.CheckIn
 
-@Database(entities = [Contact::class, CheckIn::class], version = 3, exportSchema = false)
+@Database(entities = [Contact::class, CheckIn::class], version = 4, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun contactDao(): ContactDao
     abstract fun checkInDao(): CheckInDao
