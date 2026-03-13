87fc4d30ebd177769806f8f46c3c81cd49ead158
webmancer1 <mwendwajames2004@gmail.com>
2026-03-13 13:48:55 +0300

    UI changes

--- app/src/main/java/com/example/lifeping/ui/theme/Shape.kt
+++ app/src/main/java/com/example/lifeping/ui/theme/Shape.kt
@@ -5,8 +5,8 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(8.dp),   // Buttons, inputs
-    medium = RoundedCornerShape(16.dp), // Cards
-    large = RoundedCornerShape(24.dp),  // Dialogs, specialized containers
-    extraLarge = RoundedCornerShape(32.dp) // Bottom sheets
+    small = RoundedCornerShape(12.dp),   // Buttons, inputs
+    medium = RoundedCornerShape(20.dp), // Cards
+    large = RoundedCornerShape(28.dp),  // Dialogs, specialized containers
+    extraLarge = RoundedCornerShape(36.dp) // Bottom sheets
 )
